c8d18388ae46a52c2ec4af9c4e2bdd5f2faa5c46
0x8m6z9l526 <tkacenkon907@gmail.com>
2022-01-17 15:43:32 +0400

    Платформер стал dodle-jump'ом

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import pygame
+from pygame.locals import *
+import sys
+import random
+import time
 import os
 
-pygame.init()
 
-win = pygame.display.set_mode((1350, 500))
-pygame.display.set_caption("First Game")
+
+pygame.init()
+vec = pygame.math.Vector2  # 2 for two dimensional
+
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+FramePerSec = pygame.time.Clock()
+
+pygame.mixer.music.load('soundtrack.mp3')
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play()
+
+sound1 = pygame.mixer.Sound('pryshok.mp3')
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 def load_image(name, color_key=None):
@@ -14,186 +42,209 @@ def load_image(name, color_key=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-    if color_key is -1:
-        color_key = image.get_at((0, 0))
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
-    image = image.convert_alpha()
+    else:
+        image = image.convert_alpha()
     return image
 
+bg = pygame.transform.scale(load_image('fon_dodle.png'), (WIDTH, HEIGHT))
 
-walkRight = [load_image('R1.png'), load_image('R2.png'), load_image('R3.png'),
-             load_image('R4.png'), load_image('R5.png'), load_image('R6.png'),
-             load_image('R7.png'), load_image('R8.png'), load_image('R9.png')]
-walkLeft = [load_image('L1.png'), load_image('L2.png'), load_image('L3.png'),
-            load_image('L4.png'), load_image('L5.png'), load_image('L6.png'),
-            load_image('L7.png'), load_image('L8.png'), load_image('L9.png')]
+def start_screen():
+    fon = pygame.transform.scale(load_image('Start_Fon.png'), (WIDTH, HEIGHT))
+    displaysurface.blit(fon, (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # (148, 217), (267, 271)
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and 148 <= pygame.mouse.get_pos()[0] <= 267 and 217 <= \
+                    pygame.mouse.get_pos()[1] <= 271:
+                return  # начинаем игру
+            elif event.type == pygame.MOUSEBUTTONDOWN and 148 <= pygame.mouse.get_pos()[0] <= 267 and 297 <= \
+                    pygame.mouse.get_pos()[1] <= 353:
+                print(1)
 
-bg = pygame.transform.scale(load_image('fon.png'), (1350, 500))
-char = load_image('standing_2.png')
-char = pygame.transform.scale(char, (180, 225))
-
-x = 50
-y = 150
-width = 40
-height = 60
-vel = 5
-
-clock = pygame.time.Clock()
-
-isJump = False
-jumpCount = 10
-
-left = False
-right = False
-walkCount = 0
-
-lastPos = 'right'
+        pygame.display.flip()
+        FramePerSec.tick(FPS)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.char = load_image('character.png')
+        self.surf = pygame.transform.scale(self.char, (50, 50))
+        self.surf.blit(self.char, (0, 0))
+        self.rect = self.surf.get_rect()
+        self.pos = vec((10, 360))
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.jumping = False
+        self.score = 0
+
+    def move(self):
+        self.acc = vec(0, 0.5)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+
+    def jump(self):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if hits and not self.jumping:
+            self.jumping = True
+            self.vel.y = -15
+
+    def cancel_jump(self):
+        if self.jumping:
+            if self.vel.y < -3:
+                self.vel.y = -3
+
+    def update(self):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if self.vel.y > 0:
+            if hits:
+                if self.pos.y < hits[0].rect.bottom:
+                    if hits[0].point == True:
+                        hits[0].point = False
+                        self.score += 1
+                    self.pos.y = hits[0].rect.top + 1
+                    self.vel.y = 0
+                    self.jumping = False
 
 
-class projectile:
-    def __init__(self, x, y, radius, color, facing):
-        self.x = x
-        self.y = y
-        self.r = radius
-        self.color = color
-        self.f = facing
-        self.vel = 8 * facing
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.image.load("platform.png")
+        self.rect = self.surf.get_rect(center=(random.randint(0, WIDTH - 60),
+                                               random.randint(0, HEIGHT - 30)))
+        self.speed = random.randint(-1, 2)
 
-    def draw(self, win):
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.r)
+        self.point = True
+        self.moving = True
+
+    def move(self):
+        if self.moving == True:
+            self.rect.move_ip(self.speed, 0)
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
 
 
-def redrawGameWindow():
-    global walkCount
-
-    win.blit(bg, (0, 0))
-    if walkCount + 1 >= 27:
-        walkCount = 0
-
-    if left:
-        win.blit(walkLeft[walkCount // 3], (x, y))
-        walkCount += 1
-    elif right:
-        win.blit(walkRight[walkCount // 3], (x, y))
-        walkCount += 1
+def check(platform, groupies):
+    if pygame.sprite.spritecollideany(platform, groupies):
+        return True
     else:
-        win.blit(char, (x, y))
-        walkCount = 0
-
-    for bullet in bullets:
-        bullet.draw(win)
-    pygame.display.update()
-
-
-run = True
-bullets = []
-mouse = False
-Boba = False
+        for entity in groupies:
+            if entity == platform:
+                continue
+            if (abs(platform.rect.top - entity.rect.bottom) < 40) and (
+                    abs(platform.rect.bottom - entity.rect.top) < 40):
+                return True
+        C = False
 
 
+def plat_gen():
+    while len(platforms) < 6:
+        width = random.randrange(50, 100)
+        p = platform()
+        C = True
 
-while run:
-    clock.tick(27)
+        while C:
+            p = platform()
+            p.rect.center = (random.randrange(0, WIDTH - width),
+                             random.randrange(-50, 0))
+            C = check(p, platforms)
+        platforms.add(p)
+        all_sprites.add(p)
 
+
+PT1 = platform()
+P1 = Player()
+
+PT1.surf = pygame.Surface((WIDTH, 20))
+PT1.surf.fill((255, 0, 0))
+PT1.rect = PT1.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+platforms = pygame.sprite.Group()
+platforms.add(PT1)
+
+PT1.moving = False
+PT1.point = False  ##
+
+for x in range(random.randint(4, 5)):
+    C = True
+    pl = platform()
+    while C:
+        pl = platform()
+        C = check(pl, platforms)
+    platforms.add(pl)
+    all_sprites.add(pl)
+
+start_screen()
+
+while True:
+    P1.update()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse = True
-    for bullet in bullets:
-        if bullet.x < 1350 and bullet.x > 0:
-            bullet.x += bullet.vel
-        else:
-            bullets.pop(bullets.index(bullet))
-            mouse = False
-    keys = pygame.key.get_pressed()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                P1.jump()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                P1.cancel_jump()
+                sound1.play()
 
-    if mouse:
-        if len(bullets) < 5:
-            if lastPos == 'right':
-                facing = 1
-            else:
-                facing = -1
-            bullets.append(projectile(round(x + width // 2), round(y + height // 2), 4, (255, 0, 0), facing))
+    if P1.rect.top > HEIGHT:
+        for entity in all_sprites:
+            entity.kill()
+            time.sleep(1)
+            displaysurface.fill((255, 0, 0))
+            pygame.display.update()
+            time.sleep(1)
+            pygame.quit()
+            sys.exit()
 
-    if keys[pygame.K_LEFT] and x > vel:
-        if x != 555:
-            x -= vel
-            left = True
-            right = False
-            lastPos = 'left'
-            print(x, y)
-        elif x == 355 and y < 150:
-            x -= vel
-            left = True
-            right = False
-            lastPos = 'left'
-            print(x, y)
-        if x > 355 and x < 560 and y < 60 and y > 50:
-            x -= vel
-            left = True
-            right = False
-            lastPos = 'left'
-            Boba = False
-        else:
-            Boba = True
+    if P1.rect.top <= HEIGHT / 3:
+        P1.pos.y += abs(P1.vel.y)
+        for plat in platforms:
+            plat.rect.y += abs(P1.vel.y)
+            if plat.rect.top >= HEIGHT:
+                plat.kill()
 
-    elif keys[pygame.K_RIGHT] and x < 1250 - vel - width:
-        if x != 355:
-            x += vel
-            left = False
-            right = True
-            lastPos = 'right'
-        elif x == 355 and y < 150:
-            x += vel
-            left = False
-            right = True
-            lastPos = 'right'
-        if x > 355 and x < 560 and y < 60 and y > 50:
-            x += vel
-            left = False
-            right = True
-            lastPos = 'right'
-            Boba = False
-        else:
-            Boba = True
+    plat_gen()
+    displaysurface.blit(bg, (0, 0))
+    f = pygame.font.SysFont("Verdana", 20)
+    g = f.render(str(P1.score), True, (123, 255, 0))
+    displaysurface.blit(g, (WIDTH / 2, 10))
 
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+        entity.move()
 
-        print(x, y)
-
-    else:
-        left = False
-        right = False
-        walkCount = 0
-        print(x, y)
-
-    if not isJump:
-        if keys[pygame.K_SPACE]:
-            isJump = True
-            left = False
-            right = False
-            walkCount = 0
-            print(x, y)
-    else:
-        if jumpCount >= -10:
-            if Boba:
-                y -= (jumpCount * abs(jumpCount)) * 0.3
-                jumpCount -= 1
-            if (410 < x < 525 and y <= 51):
-                if keys[pygame.K_SPACE]:
-                    isJump = True
-                    left = False
-                    right = False
-                    walkCount = 0
-                    y -= (jumpCount * abs(jumpCount)) * 0.3
-                    jumpCount -= 1
-                    print(x, y)
-        else:
-            jumpCount = 10
-            isJump = False
-    print(x, y)
-    redrawGameWindow()
-
-pygame.quit()
+    pygame.display.update()
+    FramePerSec.tick(FPS)
